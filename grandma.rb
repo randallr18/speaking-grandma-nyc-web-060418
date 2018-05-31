fefa8ce6ad767e65118f53a0b812ba17ad1126e8
Risher Randall <randallr18@mail.wlu.edu>
2018-05-31 18:56:41 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma (phrase)
   elsif phrase.upcase == phrase
     return "NO, NOT SINCE 1938!"
   else
-    return "HUH?! SPEAK UP, SONNY"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
